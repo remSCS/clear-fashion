8397dbe66122154b3ceebbfccb403f5c3cfd9a6d
Rémi LOMBARD <remi.lombard@edu.devinci.fr>
2022-03-22 18:21:26 +0100

    Changing URL

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -50,7 +50,7 @@ const setCurrentProducts = ({result, meta}) => {
 const fetchProducts = async (page = 1, size = 12) => {
   try {
     const response = await fetch(
-      `https://clear-fashion-server-side.vercel.app/products/search?limit=${page}&size=${size}`
+      `https://clear-fashion-server-side.vercel.app/products`
     );
     const body = await response.json();
 

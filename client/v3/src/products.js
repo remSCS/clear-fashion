d50df536818e98ad556df8cce2c66ed5fe88229d
Rémi <64485036+remSCS@users.noreply.github.com>
2022-04-08 09:54:25 +0200

    Updated API url for calls

--- client/v3/src/products.js
+++ client/v3/src/products.js
@@ -1,41 +1,5 @@
-
-
-// const fetchProducts = async (page = 1, size = 12) => {
-//     try {
-//       const response = await fetch(
-//         `http://localhost:8092/loadAllProductsWithPage?page=${page}&size=${size}`
-//       );
-//       const body = await response.json();
-  
-//       if (response.status !== 200) {
-//         console.error(response);
-//         return {currentProducts, currentPagination};
-//       }
-  
-//       return {result: body, meta: page};
-//     } catch (error) {
-//       console.error(error);
-//       return {currentProducts, currentPagination};
-//     }
-//   };
-
-
-
-// async function fetchProducts(page=1,size=12,brand="",price=0) {
-//     let url = "";
-//     if(price!==0 && (price===1 || price===-1)){
-//         url = `http://localhost:8092/loadClientProducts_filtered?${page ? `&page=${page}` : ""}&size=${size}&brand=${brand}&price=${price}`;
-//     }
-//     else{// aucun tri par prix
-//         url= `http://localhost:8092/loadClientProducts?page=${page}&size=${size}&brand=${brand}`;
-//     }
-    
-//     const body = await (await fetch(url)).json();
-//     return body;
-//   }
-
 async function fetchProducts(page=1,productsPerPage=12,brand="",price=0) {
-    let url = `http://localhost:8092/loadClientProducts?currentPage=${page}&productsPerPage=${productsPerPage}&brand=${brand}&price=${price}`;
+    let url = `https://clear-fashion-server-side.vercel.app//loadClientProducts?currentPage=${page}&productsPerPage=${productsPerPage}&brand=${brand}&price=${price}`;
     
     let {products,meta} = await (await fetch(url)).json();
     return {products,meta};

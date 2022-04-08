75dead4beb1e47f10cd7e596f3a9fd1949cc1232
Rémi <64485036+remSCS@users.noreply.github.com>
2022-04-08 10:01:51 +0200

    Fixed typo in API url

--- client/v3/src/products.js
+++ client/v3/src/products.js
@@ -1,9 +1,8 @@
-async function fetchProducts(page=1,productsPerPage=12,brand="",price=0) {
-    let url = `https://clear-fashion-server-side.vercel.app//loadClientProducts?currentPage=${page}&productsPerPage=${productsPerPage}&brand=${brand}&price=${price}`;
+async function fetchProducts(page=1, productsPerPage=12, brand="", price=0) {
+    let url = `https://clear-fashion-server-side.vercel.app/loadClientProducts?currentPage=${page}&productsPerPage=${productsPerPage}&brand=${brand}&price=${price}`;
     
     let {products,meta} = await (await fetch(url)).json();
     return {products,meta};
   }
- 
 
-  export default fetchProducts;
+ export default fetchProducts;
